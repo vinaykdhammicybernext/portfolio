6411249bd51085ad857ac3fe1b209c52f804caa2
Vinay Kumar <vinaykdhammi.cybernext@gmail.com>
2025-12-30 17:58:47 +0530

    designed the project detail page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
   const allProjects = getProjects();
   return (
     <section className="flex flex-col items-center">
-      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:py-16 py-4 pt-16">
+      <div className="container lg:py-16 py-4 pt-16">
         <ProjectsList projects={allProjects} />
       </div>
     </section>
